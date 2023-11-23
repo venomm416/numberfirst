9f52d8401940b17b30a6caabdc43a0a254ae4490
venomm416 <jaoprang28@gmail.com>
2023-11-23 11:23:35 +0700

    อัพเดท

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 <script>
-    var number1 = window.prompt("กรุณากรอกตัวเลขที่ 12 ");
+    var number1 = window.prompt("กรุณากรอกตัวเลขที่ 1 ");
     var number2 = window.prompt("กรุณากรอกตัวเลขที่ 2 ");
     var result = parseInt(number1) + parseInt(number2);
     alert(number1+" + "+number2+" = "+result);
